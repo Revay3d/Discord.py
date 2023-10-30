461630500e16dbc179edf5de9177ad10bb33c7e6
revayDev <99051274+Revay3d@users.noreply.github.com>
2023-10-30 16:46:39 -0500

    Update on_member_join.py

--- events/on_member_join.py
+++ events/on_member_join.py
@@ -1,34 +1,24 @@
 from discord.ext import commands
 import discord
-import random
 
-class On_message(commands.Cog):
+class On_member_remove(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bienvenidas = bot.bienvenidas
-        self.chat = bot.chat
+        self.despedidas = bot.despedidas
 
     @commands.Cog.listener()
-    async def on_member_join(self, member): 
-        embed = discord.Embed(title="Bienvenid@", description=f"""👋 ¡Hola! Bienvenido/a {member.mention}! a nuestro servidor de Discord. 👋
+    async def on_member_remove(self, member):  # Añade 'self' aquí
+        channel = self.bot.get_channel(self.despedidas)
+        if channel is not None:
+            embed = discord.Embed(title="Despedidas", description=f"""👋 ¡Adiós {member.name}! 
 
-Somos una comunidad de personas con intereses comunes. Aquí podrás socializar, aprender y divertirte.
-¡Que lo pases bien y te sientas cómodo/a! 😊 """, color=discord.Color.random())
-
-        if member.avatar:
-            embed.set_thumbnail(url=member.avatar.url)
-
-        if self.bot.user.avatar:    
+Hasta pronto, ¡cuídate mucho! 😊 """, color=self.bot.color)  # Usa self.bot.color aquí
+            if member.avatar:
+                embed.set_thumbnail(url=member.avatar.url)
+            if self.bot.user.avatar:    
              embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar.url)
-             
-        message = [f"Que bien ya llego {member.mention}", f"uff... y ¿quien eres? {member.name}",
-                   f"no ledigan a {member.mention} donde escondi las galletas..."]
 
-        channel = self.bot.get_channel(self.bienvenidas)
-        channel_Bienvenida = self.bot.get_channel(self.chat)
-
-        await channel_Bienvenida.send(random.choice(message))
-        await channel.send(embed=embed)
-
+            await channel.send(embed=embed)
+      
 async def setup(bot):
-    await bot.add_cog(On_message(bot))
+    await bot.add_cog(On_member_remove(bot))
